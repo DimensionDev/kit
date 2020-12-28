744969f13a60c20464734161a9eaf9ad143f4959
Septs <github@septs.pw>
2020-12-28 16:48:42 +0800

    redesign save as file

--- src/saveAsFile.ts
+++ src/saveAsFile.ts
@@ -1,24 +1,29 @@
-import { once } from 'lodash-es'
-
-const createDlElement = once(() => document.createElement('a'))
-
-export function saveAsFileFromUrl(url: string, fileName = '', isAndroid = true) {
+export function saveAsFileFromURL(
+  url: string,
+  fileName = '',
+  isAndroid = true,
+) {
   if (isAndroid) {
-      const dlElem = createDlElement()
-      dlElem.href = url
-      dlElem.download = fileName
-      dlElem.click()
+    const element = document.createElement('a');
+    element.href = url;
+    element.download = fileName;
+    element.click();
   } else {
-      browser.downloads.download({
-          url,
-          filename: fileName,
-          saveAs: true,
-      })
+    browser.downloads.download({
+      url,
+      filename: fileName,
+      saveAs: true,
+    });
   }
 }
 
-export function saveAsFileFromBuffer(file: BufferSource, mimeType: string, fileName = '', isAndroid = true) {
-  const blob = new Blob([file], { type: mimeType })
-  const url = URL.createObjectURL(blob)
-  saveAsFileFromUrl(url, fileName, isAndroid)
+export function saveAsFileFromBuffer(
+  parts: BufferSource[],
+  type?: string,
+  fileName = '',
+  isAndroid = true,
+) {
+  const blob = new Blob(parts, { type });
+  const url = URL.createObjectURL(blob);
+  saveAsFileFromURL(url, fileName, isAndroid);
 }
