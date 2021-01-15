5d4304b5ec3e1e0db4ccf5bc969f14028dc70e88
Septs <github@septs.pw>
2021-01-15 11:33:06 +0800

    add findAvailableImageURL method

--- src/detect.ts
+++ src/detect.ts
@@ -7,24 +7,25 @@ export function detectVideoSupport(url: string) {
 }
 
 export function detectImageSupport(url: string) {
-  return new Promise<boolean>((resolve) => {
-    const element = document.createElement('img');
-    element.addEventListener('load', () => {
-      resolve(true);
-    });
-    element.addEventListener('error', () => {
-      resolve(false);
-    });
-    element.src = url;
-  });
+  return detectMediaSupport('img', url);
 }
 
-function detectMediaSupport(name: 'audio' | 'video', url: string) {
+export async function findAvailableImageURL(srcset: ArrayLike<string>) {
+  for (const url of Array.from(srcset)) {
+    if (await detectImageSupport(url)) {
+      return url;
+    }
+  }
+  throw new Error('unavailable');
+}
+
+function detectMediaSupport(name: 'img' | 'audio' | 'video', url: string) {
   return new Promise<boolean>((resolve) => {
     const reject = () => resolve(false);
     const element = document.createElement(name);
     element.addEventListener('error', reject);
     element.addEventListener('abort', reject);
+    element.addEventListener('load', () => resolve(true));
     element.addEventListener('loadedmetadata', () => resolve(true));
     element.src = url;
   });
