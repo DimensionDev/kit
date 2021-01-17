c2304086e49951ccd782c817270c9ea72e4c5814
Hancheng Zhou <z308114274@gmail.com>
2021-01-17 20:03:28 +0800

    revamp findAvailableImageURL

--- src/detect.ts
+++ src/detect.ts
@@ -10,23 +10,31 @@ export function detectImageSupport(url: string) {
   return detectMediaSupport('img', url);
 }
 
-export async function findAvailableImageURL(srcset: ArrayLike<string>) {
-  for (const url of Array.from(srcset)) {
-    if (await detectImageSupport(url)) {
-      return url;
-    }
-  }
-  throw new Error('unavailable');
+export function findAvailableImageURL(srcset: ArrayLike<string>, timeout?: number) {
+  const promises = Array.from(srcset).map((url) => detectImageSupport(url));
+  const id =
+    timeout &&
+    setTimeout(() => {
+      throw new Error('unavailable');
+    }, timeout);
+  return new Promise((resolve) => {
+    Promise.any(promises).then((url) => {
+      id && clearTimeout(id);
+      resolve(url);
+    });
+    Promise.allSettled(promises).then((r) => {
+      if (r.every((v) => v.status === 'rejected')) throw new Error('unavailable');
+    });
+  });
 }
 
 function detectMediaSupport(name: 'img' | 'audio' | 'video', url: string) {
-  return new Promise<boolean>((resolve) => {
-    const reject = () => resolve(false);
+  return new Promise<string>((resolve, reject) => {
     const element = document.createElement(name);
     element.addEventListener('error', reject);
     element.addEventListener('abort', reject);
-    element.addEventListener('load', () => resolve(true));
-    element.addEventListener('loadedmetadata', () => resolve(true));
+    element.addEventListener('load', () => resolve(url));
+    element.addEventListener('loadedmetadata', () => resolve(url));
     element.src = url;
   });
 }
