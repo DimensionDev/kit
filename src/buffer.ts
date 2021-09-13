84c17d74f5b308e77d72da51dfae6b12ad2ac320
Jack Works <jackworks@protonmail.com>
2021-09-13 19:17:37 +0800

    feat: make concat array buffer no deps to web api

--- src/buffer.ts
+++ src/buffer.ts
@@ -25,8 +25,19 @@ export function encodeArrayBuffer(input: ArrayBuffer) {
   return btoa(encoded);
 }
 
-export function concatArrayBuffer(...parts: ArrayBuffer[]) {
-  return blobToArrayBuffer(new Blob(parts));
+export function concatArrayBufferSync(...parts: ArrayBuffer[]): ArrayBuffer {
+  const buffers = parts.map((x) => new Uint8Array(x));
+  const u8 = new Uint8Array(buffers.reduce((a, b) => a + b.length, 0));
+
+  let last = 0;
+  for (const each of buffers) {
+    u8.set(each, last);
+    last += each.length;
+  }
+  return u8.buffer;
+}
+export async function concatArrayBuffer(...parts: ArrayBuffer[]) {
+  return concatArrayBufferSync(...parts);
 }
 
 export function isEuqals(a: Uint8Array, b: Uint8Array): boolean;
