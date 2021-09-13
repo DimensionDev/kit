595fbc22d462f6f3b56ec15cf699629e4a27aafb
Jack Works <jackworks@protonmail.com>
2021-09-13 18:59:14 +0800

    fix: esm extension

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,7 @@ set -x
 rimraf dist
 npx ttsc
 tsc -p tsconfig.cjs.json
+find ./esm/** -name '*.js.map' -exec sh -c 'mv "$0" "${0%.js.map}".mjs.map' {} \;
+find ./esm/** -name '*.d.ts.map' -exec sh -c 'cp "$0" "${0%.d.ts.map}".mjs.d.ts.map' {} \;
 find ./esm/** -name '*.js' -exec sh -c 'mv "$0" "${0%.js}".mjs' {} \;
+find ./esm/** -name '*.d.ts' -exec sh -c 'cp "$0" "${0%.d.ts}".mjs.d.ts' {} \;
