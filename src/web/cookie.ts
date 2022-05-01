bcf314255af93a1d7aae662422012fc2d7a4b98b
Septs <github@septs.pw>
2022-05-01 10:24:27 +0800

    fix cookie kit

--- src/web/cookie.ts
+++ src/web/cookie.ts
@@ -1,6 +1,8 @@
 export function getCookieValue(this: string | undefined, name: string) {
     const cookie = this ?? document.cookie
-    const startOf = cookie.indexOf(`${name}=`) + name.length + 1
-    const endOf = cookie.indexOf('; ', startOf)
-    return decodeURIComponent(cookie.slice(startOf, endOf))
+    const start = cookie.indexOf(`${name}=`)
+    if (start === -1) return
+    let end: number | undefined = cookie.indexOf('; ', start)
+    if (end === -1) end = undefined
+    return decodeURIComponent(cookie.slice(start + name.length + 1, end))
 }
